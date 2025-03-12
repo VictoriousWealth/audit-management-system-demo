03bcf796fa31eaf56af817a7845c7a82140d3cf0
Jonathan Lupu <jlupu1@sheffield.ac.uk>
2025-03-12 12:59:55 +0000

    dropped db

--- db/schema.rb
+++ db/schema.rb
@@ -10,44 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_03_07_143304) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_101256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "audit_assignments", force: :cascade do |t|
-    t.integer "role"
-    t.integer "status"
-    t.datetime "time_accepted"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "audit_findings", force: :cascade do |t|
-    t.string "description"
-    t.integer "category"
-    t.integer "risk_level"
-    t.datetime "time_of_creation"
-    t.datetime "time_of_verification"
-    t.datetime "time_of_distribution"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "companies", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "contacts", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "contact_email"
-    t.string "contact_phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer "priority", default: 0, null: false
@@ -64,23 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_03_07_143304) do
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
-  create_table "electronic_signatures", force: :cascade do |t|
-    t.datetime "signed_at"
-    t.integer "signature_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "login_attempts", force: :cascade do |t|
-    t.datetime "attempt_time"
-    t.boolean "success"
-    t.string "ip_address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_login_attempts_on_user_id"
-  end
-
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
     t.text "data"
@@ -90,15 +38,4 @@ ActiveRecord::Schema[7.0].define(version: 2025_03_07_143304) do
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password"
-    t.integer "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "login_attempts", "users"
 end
