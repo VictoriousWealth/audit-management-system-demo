6dd318d95ef9f1be774f833e3a82242152520bd4
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-03-20 14:18:13 +0000

    Did pie chart with real data. Figuring out how to display percentages on pie chart using chart.js

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -2,9 +2,13 @@ import Rails from "@rails/ujs";
 import './styles'
 import 'bootstrap';
 import Chartkick from "chartkick"
-import Chart from "chart.js/auto"  // 'chart.js' alone may throw issues in newer versions
+import Chart from "chart.js/auto"  
+import ChartDataLabels from 'chartjs-plugin-datalabels'  
+Chartkick.options = {
+    colors: ["#42CA68", "#F39C12", "#E74C3C", "#8E44AD", "#3498DB"]
+};
 
+Chart.register(ChartDataLabels)                            
 Chartkick.use(Chart)
-
 
 Rails.start();
