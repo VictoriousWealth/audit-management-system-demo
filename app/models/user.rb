1ba9501ce1d564af7dd10e5474e4e41b0ae1877a
Irmak Akman <iakman1@sheffield.ac.uk>
2025-03-27 15:09:32 +0000

    Create audit closure letter, almost finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ApplicationRecord
   }
 
   has_many :audit_assignments
-  has_one :company
+  belongs_to :company, optional: true 
   has_many :audits
   has_many :electronic_signatures
   has_many :login_attempts
@@ -53,4 +53,8 @@ class User < ApplicationRecord
   has_many :audit_schedules
   has_many :audit_closure_letters
   has_many :reports
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
+  
+  resources :audits do
+    resources :audit_closure_letters, only: [:new, :create]
+  end
 end
