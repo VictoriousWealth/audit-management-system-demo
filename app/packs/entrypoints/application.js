0f4f37110632a839eb98e9eed05f0ce486d344dd
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-03-29 16:43:35 +0000

    Resolve merge conflict in application.js by consolidating imports

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -5,10 +5,7 @@ import '../scripts/audit_form';
 import '../scripts/nick_chart'
 import '../scripts/nick_calendar'
 
-<<<<<<< HEAD
-=======
 import "../scripts/nav_bar";
 //import "../scripts/show_comapany_field"
 
->>>>>>> origin
 Rails.start();
