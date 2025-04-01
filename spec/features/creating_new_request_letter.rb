b41150f06cbfe5bace6f066197da4e57dabe6a42
Jonathan Lupu <jlupu1@sheffield.ac.uk>
2025-04-01 15:23:17 +0100

    test models

--- spec/features/creating_new_request_letter.rb
+++ spec/features/creating_new_request_letter.rb
@@ -1,7 +1,19 @@
 require "rails_helper"
 
 describe "Creating a new audit request letter" do
-  specify "User can create a new audit request letter" do
+  specify "User can see the prefilled data" do
     user = User.create(email: 'manager.email.address@sheffield.ac.uk', password: 'Password123', password_confirmation: 'Password123', manager: true)
     login_as user
+    
+    audit = FactoryBot.create(:audit)
+    audit_detail = FactoryBot.create(:audit_detail, audit: audit)
+
+    visit new_audit_audit_request_letter_path(audit)
+
+    expect(page).to have_content("Audit Request Letter")
+    expect(page).to have_field("Scope", with: "The scope of test audit")
+    expect(page).to have_content("The Purpose of test audit")
+    expect(page).to have_content("Audit Scope")
+    expect(page).to have_content("Audit Purpose") 
+
 
