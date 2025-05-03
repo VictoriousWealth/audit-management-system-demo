e3ef521fdf5ca805f4d2e9a45d22347e74496e51
Nicole <nmurch1@sheffield.ac.uk>
2025-05-03 22:32:02 +0100

    Refactored Create Questionnaire JS entrypoint

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -8,8 +8,6 @@ import '../scripts/iCalendar'
 import "../scripts/nav_bar";
 import "../scripts/create_audit_closure_letter";
 
-import '../scripts/create_questionnaire';
-import "../scripts/modals_controller";
 import "@hotwired/turbo-rails";
 import { Application } from "@hotwired/stimulus"
 import ModalsController from "../scripts/modals_controller"
