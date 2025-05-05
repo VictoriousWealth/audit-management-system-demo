9c51f778f85756dff86ab54d1e635cd88a61b6de
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-05-05 20:46:36 +0100

    chore: fixed schema due to migration

--- db/schema.rb
+++ db/schema.rb
@@ -362,7 +362,7 @@ ActiveRecord::Schema[7.0].define(version: 202503309183408) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.integer "failed_attempts", default: 0, null: false
+    t.integer "failed_attempts"
     t.string "unlock_token"
     t.datetime "locked_at"
     t.index ["company_id"], name: "index_users_on_company_id"
