895ee52a13c3c1b984a0cd29a9f5f926492cdd40
Jonathan Lupu <jlupu1@sheffield.ac.uk>
2025-05-15 10:58:45 +0100

    deployemnt config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   # Sentry is only enabled when the dsn is set.
   unless Rails.env.development? || Rails.env.test?
-    config.dsn = 'REPLACE_WITH_YOUR_DSN'
+    config.dsn = 'https://0f65d610387ba6d764c09366ab42b8ed@sentry.shefcompsci.org.uk/24'
   end
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.before_send = -> (event, hint) { ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters).filter(event.to_hash) }
